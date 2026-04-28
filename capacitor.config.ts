ee4d5a96346e8c24e409eb1332d64ea80569522f
mohamed.khalid@caelum-group.com <mohamed.khalid@caelum-group.com>
2026-04-28 15:36:03 +0300

    Add android icon and splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'com.savewise.app',
   appName: 'save wise',
   webDir: 'dist/money-calculator/browser',
+  server: {
+    androidScheme: 'https',
+  },
+  plugins: {
+    Assets: {
+      assetsDir: 'assets',
+    },
+  },
 };
 
 export default config;
