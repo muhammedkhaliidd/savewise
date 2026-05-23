436f7a05a8aaa1cc9d4c3cdc28f6212e7de398d7
mohamed.khalid@caelum-group.com <mohamed.khalid@caelum-group.com>
2026-05-23 20:53:00 +0300

    Auto-generated commit

---
+++ chunk-QH5TSJQ3.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-WAYEUNPC.js";import"./chunk-TDDKBJBY.js";import"./chunk-C6Q5SG76.js";export{a as App};
---
+++ chunk-WAYEUNPC.js
@@ -0,0 +1,1 @@
+import{b as p}from"./chunk-TDDKBJBY.js";var o=p("App",{web:()=>import("./chunk-SINHWQIH.js").then(e=>new e.AppWeb)});export{o as a};
